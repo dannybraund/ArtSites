41fb6257c632e899d2c261da201994544c3890e0
dannybraund <db559@kent.ac.uk>
2018-01-14 14:15:48 +0000

    Site 1 and 2
    Site 1 and 2 - test pages.

---
+++ Site1/zoom.js
@@ -0,0 +1,37 @@
+﻿var box;
+
+function zoomIn()
+{
+  var filename = this.src.split( "_thumb.png");
+  box.style.background="url("+filename[0]+ ".png)";
+}
+
+
+function zoomOut()
+{
+    box.style.background="inherit";
+}
+
+
+function init()
+{
+ box=document.getElementById("zoomBox");
+ 
+ var ichigo = document.getElementById("ichigo");
+ ichigo.onmouseover=zoomIn;
+ ichigo.onmouseout=zoomOut; 
+ 
+ var kindred = document.getElementById("kindred");
+ kindred.onmouseover=zoomIn;
+ kindred.onmouseout=zoomOut; 
+ 
+ var darker = document.getElementById("darker");
+ darker.onmouseover=zoomIn;
+ darker.onmouseout=zoomOut; 
+ 
+ var Kaneki = document.getElementById("Kaneki");
+ Kaneki.onmouseover=zoomIn;
+ Kaneki.onmouseout=zoomOut;
+
+}
+document.addEventListener("DOMContentLoaded", init , false ) ;
